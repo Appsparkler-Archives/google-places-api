d9b0e312a77cfe86b489c928c63e364dd89ae046
Aakash Shah <subs@appsparkler.com>
2021-05-04 04:40:56 +0530

    clean: removed sample code

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import express from 'express'
 import citiesMiddleware from './middlewares/cities'
+import dotenv from 'dotenv'
 
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.use('/api/cities/:query', citiesMiddleware)
 

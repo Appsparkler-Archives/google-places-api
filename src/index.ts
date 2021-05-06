4730e1c15c757db2eaaf42bcc41635df8976955e
Aakash Shah <subs@appsparkler.com>
2021-05-07 04:39:18 +0530

    refactor: moved morgan-middleware to seperate module

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import express, {RequestHandler} from 'express'
 import corsMiddleware from './middlewares/cors'
-import morgan from 'morgan'
 import citiesMiddleware from './middlewares/cities'
+import morganMiddleware from './middlewares/morgan'
 import './config'
 
 const app = express()
 const port = process.env.PORT
 
 const helloMiddleware:RequestHandler = (req, res) => res.send('Hello! Welcome To Cities API')
-const morganMiddleware = morgan('tiny')
 app.use(morganMiddleware)
 app.use(corsMiddleware(process.env))
 app.use('/api/cities/:query', citiesMiddleware)

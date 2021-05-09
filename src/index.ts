b17d68cd367f0a1028e1eb54ea1c413c979094c6
Aakash Shah <subs@appsparkler.com>
2021-05-09 10:28:14 +0530

    chore: updated setup for netlify

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,13 @@ import express, {RequestHandler} from 'express'
 import corsMiddleware from './middlewares/cors'
 import morganMiddleware from './middlewares/morgan'
 import path from 'path'
-import bodyParser from 'body-parser'
 import serverless from 'serverless-http'
-// import router from '
 import router from './routes'
 import './config'
 
 const app = express()
-const port = process.env.PORT
 
 app.use(router)
-app.use(bodyParser.json());
 app.use(morganMiddleware)
 app.use(corsMiddleware(process.env))
 app.use('/.netlify/functions/index', router)
